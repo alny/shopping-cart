5879868dc2ca98c8228bf8186fdd8e4a023ccdb3
drixmix <foxyhorse@gmail.com>
2016-11-16 13:23:39 +0100

    Added Cart Logic

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 
+var Cart = require('../models/cart');
 var Product = require('../models/products');
 
 
@@ -20,6 +21,29 @@ router.get('/', function(req, res, next) {
   })
 
 });
+
+router.get('/add-to-cart/:id', function(req, res, next){
+      var productId = req.params.id;
+      var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+
+      Product.findById(productId, function(err, product){
+        if(err){
+          return res.redirect('/')
+        }
+        cart.add(product, productId);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/')
+      });
+});
+
+router.get('/shopping-cart', function(req, res){
+  if(!req.session.cart){
+    return res.render('shop/shopping-cart', {products: null});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+})
 
 
 
